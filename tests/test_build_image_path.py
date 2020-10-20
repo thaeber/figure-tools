e75d41d0e2f4a60e5ca16acd7106f188b3957439
thaeber <40823270+thaeber@users.noreply.github.com>
2020-10-20 13:28:08 +0200

    Store images in common locations (#15)

---
+++ tests/test_build_image_path.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+from figure_tools.save import build_image_path
+
+import pytest
+
+
+def test_unmodified_path():
+    assert build_image_path('test/hallo.py') == Path('test/hallo.py')
+
+
+def test_env_image_path_wo_workspace(monkeypatch):
+    monkeypatch.setenv('FIG_TOOLS_IMAGE_PATH', 'a/images')
+    assert build_image_path('a/b/c/d/test.py') == Path('a/images/test.py')
+
+
+def test_env_image_path_with_workspace(monkeypatch):
+    monkeypatch.setenv('FIG_TOOLS_IMAGE_PATH', 'a/images')
+    monkeypatch.setenv('FIG_TOOLS_WORKSPACE_ROOT', 'a/b')
+    assert build_image_path('a/b/c/d/test.py') == Path('a/images/c/d/test.py')
+
+
+def test_env_workspace_wo_image_path(monkeypatch):
+    monkeypatch.setenv('FIG_TOOLS_WORKSPACE_ROOT', 'a/b')
+    assert build_image_path('a/b/c/d/test.py') == Path('a/b/c/d/test.py')
---
+++ tests/test_save_figure.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+from figure_tools import create_figure, save_figure
+
+
+def test_save_current_figure(tmp_path: Path):
+    # create test figure
+    fig = create_figure(width='8cm', aspect_ratio=1)
+    x = np.linspace(0, 10, 100)
+    plt.plot(x, np.sin(x))
+    plt.title('test plot')
+
+    filename = tmp_path / 'a/b/test.py'
+    save_figure(filename)  # by default png-files are created
+
+    # make sure the file exists
+    assert filename.with_suffix('.png').exists()
+
+
+def test_save_figure_with_image_path(tmp_path: Path, monkeypatch):
+    # create test figure
+    fig = create_figure(width='8cm', aspect_ratio=1)
+    x = np.linspace(0, 10, 100)
+    plt.plot(x, np.sin(x))
+    plt.title('test plot')
+
+    # setup environment variables
+    image_path = tmp_path / 'images'
+    monkeypatch.setenv('FIG_TOOLS_IMAGE_PATH', str(image_path))
+
+    filename = tmp_path / 'a/b/test.py'
+    save_figure(filename)  # by default png-files are created
+
+    # make sure the file exists
+    assert (image_path / filename.name).with_suffix('.png').exists()
+
+
+def test_save_figure_with_image_path_and_workspace(tmp_path: Path,
+                                                   monkeypatch):
+    # create test figure
+    fig = create_figure(width='8cm', aspect_ratio=1)
+    x = np.linspace(0, 10, 100)
+    plt.plot(x, np.sin(x))
+    plt.title('test plot')
+
+    # setup environment variables
+    monkeypatch.setenv('FIG_TOOLS_IMAGE_PATH', str(tmp_path / 'images'))
+    monkeypatch.setenv('FIG_TOOLS_WORKSPACE_ROOT', str(tmp_path / 'a/b'))
+
+    filename = tmp_path / 'a/b/c/d/test.py'
+    save_figure(filename)  # by default png-files are created
+
+    # make sure the file exists
+    assert (tmp_path / 'images/c/d' /
+            filename.name).with_suffix('.png').exists()
